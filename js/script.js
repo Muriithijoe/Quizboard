c0861eed44f081acfc7b9300e98aff77a38c37cb
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-08-17 20:25:05 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//businesslogic
+//:businesslogic
+$.(document).ready(function(){
+  $("form#options").submit(function(event){
+
+  var question1=$("input:radio[name=question1]:checked").val();
+  var question2=$("input:radio[name=question2]:checked").val();
+  var question3=$("input:radio[name=question3]:checked").val();
+  var result= parseInt(question1)+parseInt(question2)+parseInt(question3)
+  $("#result").text("Your total score is "+result);
+
+
+  $(".quiz").fadeOut();
+  $(".answer").fadeIn();
+
+  event.preventDefault();
+});
+
+});
